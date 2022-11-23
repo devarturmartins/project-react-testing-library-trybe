dfa26360f2b976e7b9092974701aa8185cc713dc
devarturmartins <arturvl1@hotmail.com>
2022-11-23 15:20:45 -0300

    req. 3 - acho que faltando teste

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+
+describe('Testa o componente FavoritePokemon', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos;', () => {
+    renderWithRouter(<FavoritePokemon />);
+    const msg = screen.getByText(/no favorite pokémon found/i);
+    expect(msg).toBeInTheDocument();
+  });
+});
